bd149de424fb92521429951b3dac8f1256cce9e3
ZTimeGaming <ztimeaus@gmail.com>
2026-02-11 15:09:39 -0500

    solution for part2

--- homework_3_guessing_game/src/part2.rs
+++ homework_3_guessing_game/src/part2.rs
@@ -7,6 +7,21 @@ pub struct Part2 {}
 impl Strategy for Part2 {
     fn guess_the_number(player: &mut Player, min: u32, max: u32) -> u32 {
         // YOUR SOLUTION GOES HERE.
-        todo!("No solution provided yet")
+        let mut upper_bound = max;
+        let mut lower_bound = min;
+        let mut midpoint = (upper_bound + lower_bound)/2;
+        for i in min..max {
+            let mut guess = player.ask_to_compare(midpoint);
+            if guess == 0 {
+                return midpoint;
+            } else if guess == -1 {
+                upper_bound = midpoint + 1;
+                midpoint = upper_bound / 2;
+            } else if guess == 1 {
+                lower_bound = midpoint - 1;
+                midpoint = (lower_bound + upper_bound) / 2;
+            }
+        }
+        return 0;
     }
 }
