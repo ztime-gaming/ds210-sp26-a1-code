9d4f83d9c0ef4eb3069f2d15401c6be57199c201
ZTimeGaming <quaveringscores@gmail.com>
2026-03-09 23:50:57 -0400

    Update lib.rs

--- project_1_vector/fast_vec/src/lib.rs
+++ project_1_vector/fast_vec/src/lib.rs
@@ -72,30 +72,30 @@ impl<T> FastVec<T> {
 
     // Student 2 should implement this.
     pub fn push(&mut self, t: T) {
-    unsafe {
-        if self.len == self.capacity {
-            let new_capacity = self.capacity * 2;
-            let new_ptr = MALLOC.malloc(size_of::<T>() * new_capacity) as *mut T;
-
-            for i in 0..self.len {
-                let val = ptr::read(self.ptr_to_data.add(i));
-                ptr::write(new_ptr.add(i), val);
+        unsafe {
+            if self.len == self.capacity {
+                let new_capacity = self.capacity * 2;
+                let new_ptr = MALLOC.malloc(size_of::<T>() * new_capacity) as *mut T;
+    
+                for i in 0..self.len {
+                    let val = ptr::read(self.ptr_to_data.add(i));
+                    ptr::write(new_ptr.add(i), val);
+                }
+    
+                MALLOC.free(self.ptr_to_data as *mut u8);
+    
+                self.ptr_to_data = new_ptr;
+                self.capacity = new_capacity;
+    
+                ptr::write(self.ptr_to_data.add(self.len), t);
+                self.len += 1;
+    
+            } else {
+                ptr::write(self.ptr_to_data.add(self.len), t);
+                self.len += 1;
             }
-
-            MALLOC.free(self.ptr_to_data as *mut u8);
-
-            self.ptr_to_data = new_ptr;
-            self.capacity = new_capacity;
-
-            ptr::write(self.ptr_to_data.add(self.len), t);
-            self.len += 1;
-
-        } else {
-            ptr::write(self.ptr_to_data.add(self.len), t);
-            self.len += 1;
         }
     }
-}
 
     // Student 1 should implement this.
     pub fn remove(&mut self, i: usize) {
@@ -120,17 +120,16 @@ impl<T> FastVec<T> {
     // Hint: check out case 2 in memory.rs, which you can run using
     //       cargo run --bin memory
     pub fn clear(&mut self) {
-    unsafe {
+        unsafe {
             for i in 0..self.len {
                 ptr::read(self.ptr_to_data.add(i));
             }
+        }
+        MALLOC.free(self.ptr_to_data as *mut u8);
+        self.ptr_to_data = null_mut();
+        self.len = 0;
+        self.capacity = 0;
     }
-    MALLOC.free(self.ptr_to_data as *mut u8);
-    self.ptr_to_data = null_mut();
-    self.len = 0;
-    self.capacity = 0;
-}
-}
 
 // Destructor should clear the fast_vec to avoid leaking memory.
 impl<T> Drop for FastVec<T> {
